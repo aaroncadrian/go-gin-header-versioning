84451ec98291260d6bf534eb8dbe2bd6eedad5cd
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 16:48:12 -0500

    Remove unused function

--- versioning/map.go
+++ versioning/map.go
@@ -3,9 +3,3 @@ package versioning
 import "github.com/gin-gonic/gin"
 
 type VersionMap = map[string]gin.HandlerFunc
-
-func (m VersionMap) GetHandler(version string) (gin.HandlerFunc, bool) {
-	handlerFunc, ok := m[version]
-
-	return handlerFunc, ok
-}
