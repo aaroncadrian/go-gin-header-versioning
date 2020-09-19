8be8373b3212e42c32d6c46d3813d22545d4a037
Aaron Adrian <aaroncadrian@gmail.com>
2020-09-19 16:32:41 -0500

    Add getHandler function

--- versioning/map.go
+++ versioning/map.go
@@ -3,3 +3,9 @@ package versioning
 import "github.com/gin-gonic/gin"
 
 type VersionMap = map[string]gin.HandlerFunc
+
+func (m VersionMap) getHandler(version string) (gin.HandlerFunc, bool) {
+	handlerFunc, ok := m[version]
+
+	return handlerFunc, ok
+}
